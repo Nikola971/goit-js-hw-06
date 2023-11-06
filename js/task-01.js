27e4ef494c4a719ebf21cb96a8b2fd5b0a8bd6c7
Nikola971 <mukola.milyan@outlook.com>
2023-11-06 20:30:07 +0200

    esfesf

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,4 @@
-const categoriesList = document.getElementById("categories");
-const categoryItems = categoriesList.getElementsByTagName("li");
 
-console.log(`Number of categories: ${categoryItems.length}`);
+const categoriesList = document.getElementById('categories');
 
-Array.from(categoryItems).forEach((categoryItem) => {
-  const categoryNameElement = categoryItem.getElementsByTagName("h2")[0];
-  const categoryName = categoryNameElement.textContent;
-  const categoryElements = categoryItem.getElementsByTagName("ul")[0].getElementsByTagName("li");
-
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${categoryElements.length}`);
-});
+console.log(categoriesList)
--- js/task-02.js
+++ js/task-02.js
@@ -1,15 +1,18 @@
+// Масив інгредієнтів
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments",
 ];
-const ingredientsList = document.getElementById("ingredients");
+const ingredientsList = document.getElementById('ingredients');
+const fragment = document.createDocumentFragment();
 ingredients.forEach((ingredient) => {
-  const liElement = document.createElement("li");
+  const liElement = document.createElement('li');
   liElement.textContent = ingredient;
-  liElement.classList.add("item");
-  ingredientsList.appendChild(liElement);
+  liElement.classList.add('item');
+  fragment.appendChild(liElement);
 });
+ingredientsList.appendChild(fragment);
