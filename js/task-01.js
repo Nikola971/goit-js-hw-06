c6bbf117f43f73fae067ee951bd0ee0701ef424a
Nikola971 <mukola.milyan@outlook.com>
2023-11-07 11:13:20 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,12 @@
-
 const categoriesList = document.getElementById('categories');
+const categoryItems = categoriesList.getElementsByClassName('item');
 
-console.log(categoriesList)
+console.log(`Number of categories: ${categoryItems.length}`);
+
+for (const categoryItem of categoryItems) {
+  const categoryTitle = categoryItem.getElementsByTagName('h2')[0].textContent;
+  const categoryElements = categoryItem.getElementsByTagName('li').length;
+
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${categoryElements}`);
+}
